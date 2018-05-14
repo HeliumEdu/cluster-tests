d497223fe0825049272ec88b5370167e5ca172b9
Alex Laird <alexdlaird@gmail.com>
2018-05-13 21:42:59 -0700

    Fixing CI tests.

--- src/utils/workspacehelper.py
+++ src/utils/workspacehelper.py
@@ -1,5 +1,3 @@
-import json
-
 import requests
 
 from . import emailhelper
@@ -14,7 +12,9 @@ def init_workspace(response, env_api_host, username, email, password):
                              verify=False)
 
     # The test user already exists, which means a previous tests didn't clean up
-    if response.status_code == 400 and 'account is not active' in response.content:
+    json_response = response.json()
+    if response.status_code == 400 and 'non_field_errors' in json_response and \
+                    'account is not active' in json_response['non_field_errors'][0]:
         verification_code = emailhelper.get_verification_code(response, username)['verification_code']
 
         requests.get("{}/auth/user/verify/?username={}&code={}&welcome-email=false".format(env_api_host,
@@ -27,10 +27,8 @@ def init_workspace(response, env_api_host, username, email, password):
                                  verify=False)
 
     if response.status_code == 200:
-        token = json.loads(response.content)['token']
-
         requests.delete(env_api_host + '/auth/user/',
-                        headers={'Authorization': "Token " + token},
+                        headers={'Authorization': "Token " + response.json()['token']},
                         data={'username': username, 'email': email, 'password': password},
                         verify=False)
 
