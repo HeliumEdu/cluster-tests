2fd42fcb6cc85c2b99ab01bc62b6d0545f42171a
Alex Laird <contact@alexlaird.com>
2021-02-15 11:50:42 -0800

    Switch to Chrome driver.

--- src/selenium/utils/seleniumtestcase.py
+++ src/selenium/utils/seleniumtestcase.py
@@ -3,7 +3,7 @@ import unittest
 
 import requests
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 __author__ = 'Alex Laird'
 __copyright__ = 'Copyright 2018, Helium Edu'
@@ -13,8 +13,7 @@ __version__ = '1.4.26'
 class SeleniumTestCase(unittest.TestCase):
     def setUp(self):
         options = Options()
-        # options.add_argument('-headless')
-        self.driver = webdriver.Firefox(firefox_options=options)
+        self.driver = webdriver.Chrome(options=options)
 
         self.app_host = os.environ.get('PROJECT_APP_HOST')
         self.api_host = os.environ.get('PROJECT_API_HOST')
