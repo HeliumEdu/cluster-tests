5ed241646a1a30279b3ca14239c26005e0c26eee
Alex Laird <contact@alexlaird.com>
2021-02-15 17:30:35 -0800

    [heliumcli] Release 1.4.46

--- src/selenium/utils/seleniumtestcase.py
+++ src/selenium/utils/seleniumtestcase.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 
 __author__ = 'Alex Laird'
 __copyright__ = 'Copyright 2018, Helium Edu'
-__version__ = '1.4.26'
+__version__ = "1.4.46"
 
 
 class SeleniumTestCase(unittest.TestCase):
