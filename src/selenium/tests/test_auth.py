356b183ab93181003a80e838ec12c9c2772fc0e6
Alex Laird <contact@alexlaird.com>
2024-10-16 07:52:43 -0500

    Add some frontend and Selenium tests to validate.

--- src/selenium/tests/test_auth.py
+++ src/selenium/tests/test_auth.py
@@ -2,16 +2,50 @@ __copyright__ = "Copyright (c) 2018 Helium Edu"
 __license__ = "MIT"
 __version__ = "1.5.1"
 
+import os
 import unittest
 
+from selenium.webdriver.common.by import By
 from utils.seleniumtestcase import SeleniumTestCase
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT')
+ENVIRONMENT_PREFIX = f'{ENVIRONMENT}.' if 'prod' not in ENVIRONMENT else ''
 
 
 class TestSeleniumAuth(SeleniumTestCase):
-    # TODO: add a test that registers
+    def test_register_new_user(self):
+        self.driver.get(os.path.join(self.app_host, 'register'))
+
+        username_field = self.driver.find_element(By.ID, "id_username")
+        email_field = self.driver.find_element(By.ID, "id_email")
+        password_field = self.driver.find_element(By.ID, "id_password1")
+        confirm_password_field = self.driver.find_element(By.ID, "id_password2")
+
+        username_field.send_keys("heliumedu-ci-selenium-test")
+        email_field.send_keys(f'heliumedu-ci-test@{ENVIRONMENT_PREFIX}heliumedu.dev')
+        password_field.send_keys("test_pass_1!")
+        confirm_password_field.send_keys("test_pass_1!")
+
+        login_button = self.driver.find_element(By.XPATH, "//*[@id=\"register-form\"]/div[2]/div/button")
+
+        login_button.click()
+
+        self.driver.quit()
+
+    def test_delete_user(self):
+        self.driver.get(os.path.join(self.app_host, 'settings'))
+
+        password_field = self.driver.find_element(By.ID, "delete-account")
+        password_field.send_keys("test_pass_1!")
+
+        delete_button = self.driver.find_element(By.XPATH, "/html/body/div[4]/div/div/div[3]/button[2]")
+
+        delete_button.click()
+
+        self.driver.quit()
+
     # TODO: add a test that logs in / logs out
     # TODO: add a test that ensures that /planner/calendar renders the example schedule
-    pass
 
 
 if __name__ == '__main__':
--- src/selenium/tests/test_redirects.py
+++ src/selenium/tests/test_redirects.py
@@ -2,39 +2,41 @@ __copyright__ = "Copyright (c) 2018 Helium Edu"
 __license__ = "MIT"
 __version__ = "1.6.4"
 
-
 import os
 import unittest
 
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from utils.seleniumtestcase import SeleniumTestCase
 
 
 class TestSeleniumRedirects(SeleniumTestCase):
     def test_support_redirect(self):
-        # UserVoice has shutdown their free service, commenting out test until another solution is used
-        pass
-        # info = self.get_info()
+        info = self.get_info()
 
-        # self.driver.get(os.path.join(self.app_host, 'support'))
+        self.driver.get(os.path.join(self.app_host, 'support'))
         # The /support URL redirects to an external portal
-        # WebDriverWait(self.driver, 10).until(
-        #     EC.title_is("Support for Helium")
-        # )
-        # self.assertEquals(info['support_url'], self.driver.current_url.strip('/'))
+        WebDriverWait(self.driver, 10).until(
+            EC.title_is("Home · HeliumEdu/platform Wiki")
+        )
+        self.assertEquals(info['support_url'], self.driver.current_url.strip('/'))
 
-    def test_contact_redirect(self):
-        # UserVoice has shutdown their free service, commenting out test until another solution is used
-        pass
-        # info = self.get_info()
-
-        # self.driver.get(os.path.join(self.app_host, 'contact'))
+    def test_docs_redirect(self):
+        self.driver.get(os.path.join(self.app_host, 'docs'))
         # The /support URL redirects to an external portal
-        # WebDriverWait(self.driver, 10).until(
-        #     EC.title_is("Support for Helium")
-        # )
-        # self.assertEquals(info['support_url'], self.driver.current_url.strip('/'))
+        WebDriverWait(self.driver, 10).until(
+            EC.title_is("Helium API Documentation")
+        )
+        self.assertEquals(os.path.join(self.api_host, 'docs'), self.driver.current_url.strip('/'))
+
+    def test_status_redirect(self):
+        start_url = os.path.join(self.app_host, 'status')
+        self.driver.get(start_url)
+        # The /support URL redirects to an external portal
+        WebDriverWait(self.driver, 10).until(
+            self.driver.current_url != start_url
+        )
+        self.assertEquals(os.path.join(self.api_host, 'status'), self.driver.current_url.strip('/'))
 
 
 if __name__ == '__main__':
