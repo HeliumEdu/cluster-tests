0d3e52a72592d7b68880f190490728aedb9f8309
Alex Laird <contact@alexlaird.com>
2025-09-25 16:23:39 -0500

    Add cluster test to ensure assets are deployed.

--- src/tavern/conftest.py
+++ src/tavern/conftest.py
@@ -13,3 +13,13 @@ def access_token():
                              verify=False)
 
     return response.json()["access"]
+
+
+@pytest.fixture
+def assets_prefix():
+    api_host = os.environ.get('PROJECT_API_HOST')
+
+    response = requests.get(f"{api_host}/info/",
+                             verify=False)
+
+    return response.json()["version"] + "."
