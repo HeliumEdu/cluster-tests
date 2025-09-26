aa3245a873570615a92d290257c1130e4175456e
Alex Laird <contact@alexlaird.com>
2025-09-26 08:45:08 -0500

    Chrome headless stability

--- src/selenium/seleniumtestcase.py
+++ src/selenium/seleniumtestcase.py
@@ -37,9 +37,10 @@ logger = logging.getLogger(__name__)
 class SeleniumTestCase(unittest.TestCase):
     def setUp(self):
         options = Options()
-        options.add_argument('--headless')
-        options.add_argument("--start-maximized")
+        options.add_argument('--headless=new')
+        options.add_argument('--start-maximized')
         options.add_argument('--no-sandbox')
+        options.add_argument('--disable-gpu')
         options.set_capability('goog:loggingPrefs', {'browser': 'ALL'})
 
         self.driver = webdriver.Chrome(options=options)
