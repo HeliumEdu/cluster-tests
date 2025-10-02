5474f74c9fead1fc8f93440b55dabe05a8f8ec5d
Alex Laird <contact@alexlaird.com>
2025-10-02 00:15:35 -0500

    Take advantage of plus rule for cluster test accounts.

--- src/tavern/conftest.py
+++ src/tavern/conftest.py
@@ -9,7 +9,7 @@ def access_token():
     api_host = os.environ.get('PROJECT_API_HOST')
 
     response = requests.post(f"{api_host}/auth/token/",
-                             data={"username": "heliumedu-cluster1", "password": "test_pass_1!"},
+                             data={"username": "heliumedu-cluster-1", "password": "test_pass_1!"},
                              verify=False)
 
     return response.json()["access"]
