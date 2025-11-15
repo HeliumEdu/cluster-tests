d503832e84c52f3e40d7c9b76c5bedf3506dd337
Alex Laird <contact@alexlaird.com>
2025-11-14 21:57:52 -0600

    Fix cluster test titles from SEO changes.

--- src/selenium/tests/test_unauth_pages.py
+++ src/selenium/tests/test_unauth_pages.py
@@ -14,7 +14,7 @@ from src.selenium.seleniumtestcase import SeleniumTestCase
 class TestSeleniumUnauthPages(SeleniumTestCase):
     def test_index(self):
         self.driver.get(self.app_host)
-        self.assertEqual("{} | {}".format(self.info['name'], self.info['tagline']), self.driver.title)
+        self.assertEqual("{} — {}".format(self.info['name'], self.info['tagline']), self.driver.title)
 
         WebDriverWait(self.driver, 30).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, "#tour-carousel > div > div:nth-child(1) > img"))
@@ -26,7 +26,7 @@ class TestSeleniumUnauthPages(SeleniumTestCase):
 
     def test_about(self):
         self.driver.get(os.path.join(self.app_host, 'about'))
-        self.assertEqual("{} | {}".format(self.info['name'], 'About'), self.driver.title)
+        self.assertEqual("{} | {}".format('About', self.info['name']), self.driver.title)
 
         WebDriverWait(self.driver, 30).until(
             EC.visibility_of_element_located(
@@ -39,7 +39,7 @@ class TestSeleniumUnauthPages(SeleniumTestCase):
 
     def test_press(self):
         self.driver.get(os.path.join(self.app_host, 'press'))
-        self.assertEqual("{} | {}".format(self.info['name'], 'Press'), self.driver.title)
+        self.assertEqual("{} | {}".format('Press', self.info['name']), self.driver.title)
 
         WebDriverWait(self.driver, 30).until(
             EC.visibility_of_element_located(
@@ -53,7 +53,7 @@ class TestSeleniumUnauthPages(SeleniumTestCase):
     def test_privacy(self):
         self.driver.get(os.path.join(self.app_host, 'privacy'))
 
-        self.assertEqual("{} | {}".format(self.info['name'], 'Privacy Policy'), self.driver.title)
+        self.assertEqual("{} | {}".format('Privacy Policy', self.info['name']), self.driver.title)
 
         WebDriverWait(self.driver, 30).until(
             EC.visibility_of_element_located(
@@ -67,7 +67,7 @@ class TestSeleniumUnauthPages(SeleniumTestCase):
     def test_terms(self):
         self.driver.get(os.path.join(self.app_host, 'terms'))
 
-        self.assertEqual("{} | {}".format(self.info['name'], 'Terms of Service'), self.driver.title)
+        self.assertEqual("{} | {}".format('Terms of Service', self.info['name']), self.driver.title)
 
         WebDriverWait(self.driver, 30).until(
             EC.visibility_of_element_located(
