e29985d8ffbdbee8f000e2ca680e30c61a85f32b
Alex Laird <contact@alexlaird.com>
2026-03-02 17:02:46 -0600

    Update emailhelper.py

--- src/utils/emailhelper.py
+++ src/utils/emailhelper.py
@@ -18,7 +18,6 @@ _RETRIES = 12 * 5
 
 _RETRY_DELAY = 5
 
-
 def get_verification_code(response, username, retry=0):
     environment = os.environ.get('ENVIRONMENT')
     s3 = boto3.resource('s3',
@@ -26,54 +25,66 @@ def get_verification_code(response, username, retry=0):
                         aws_secret_access_key=os.environ.get('AWS_INTEGRATION_S3_SECRET_ACCESS_KEY'),
                         region_name=os.environ.get('AWS_REGION'))
     bucket = s3.Bucket('heliumedu-integration')
+    prefix = f'{environment}/inbound.email/heliumedu-cluster/'
 
-    latest_key = None
-    for key in bucket.objects.filter(Prefix=f'{environment}/inbound.email/heliumedu-cluster/'):
-        if not latest_key or key.last_modified > latest_key.last_modified:
-            latest_key = key
+    try:
+        latest_key = None
+        for key in bucket.objects.filter(Prefix=prefix):
+            if not latest_key or key.last_modified > latest_key.last_modified:
+                latest_key = key
 
-    if latest_key is None:
+        if latest_key is None:
+            if retry < _RETRIES:
+                time.sleep(_RETRY_DELAY)
+
+                return get_verification_code(response, username, retry + 1)
+            else:
+                raise TestFailError(
+                    "The verification email was not received after {} seconds.".format(_RETRIES * _RETRY_DELAY))
+
+        logger.info('latest_key: {}'.format(latest_key))
+
+        email_str = latest_key.get()["Body"].read().decode('utf-8')
+
+        email_body = None
+        for part in Parser().parsestr(email_str).walk():
+            if part.get_content_type() == 'text/plain':
+                email_body = part.get_payload()
+                break
+
+        email_date = latest_key.last_modified
+        now = datetime.datetime.now(pytz.utc)
+        left_window = now - datetime.timedelta(seconds=15 + (retry * _RETRY_DELAY))
+        right_window = now + datetime.timedelta(seconds=15 + (retry * _RETRY_DELAY))
+
+        logger.info('left_window: {}'.format(left_window))
+        logger.info('email_date: {}'.format(email_date))
+        logger.info('right_window: {}'.format(right_window))
+
+        in_test_window = left_window <= email_date <= right_window
+        if not email_body or not in_test_window or 'username={}&code'.format(username) not in email_body:
+            if retry < _RETRIES:
+                time.sleep(_RETRY_DELAY)
+
+                return get_verification_code(response, username, retry + 1)
+            else:
+                raise TestFailError("No matching verification email could be validated after {} seconds.".format(_RETRIES * _RETRY_DELAY))
+
+        verification_code = email_body.split('verify?username={}&code='.format(username))[1].split('\n')[0].strip()
+
+        latest_key.delete()
+
+        response = {"email_verification_code": verification_code}
+        logger.info(response)
+
+        return response
+    except TestFailError:
+        raise
+    except Exception as e:
+        logger.warning('Error during email fetch: {}'.format(e))
         if retry < _RETRIES:
             time.sleep(_RETRY_DELAY)
 
             return get_verification_code(response, username, retry + 1)
         else:
-            raise TestFailError(
-                "The verification email was not received after {} seconds.".format(_RETRIES * _RETRY_DELAY))
-
-    logger.info('latest_key: {}'.format(latest_key))
-
-    email_str = latest_key.get()["Body"].read().decode('utf-8')
-
-    email_body = None
-    for part in Parser().parsestr(email_str).walk():
-        if part.get_content_type() == 'text/plain':
-            email_body = part.get_payload()
-            break
-
-    email_date = latest_key.last_modified
-    now = datetime.datetime.now(pytz.utc)
-    left_window = now - datetime.timedelta(seconds=15 + (retry * _RETRY_DELAY))
-    right_window = now + datetime.timedelta(seconds=15 + (retry * _RETRY_DELAY))
-
-    logger.info('left_window: {}'.format(left_window))
-    logger.info('email_date: {}'.format(email_date))
-    logger.info('right_window: {}'.format(right_window))
-
-    in_test_window = left_window <= email_date <= right_window
-    if not email_body or not in_test_window or 'username={}&code'.format(username) not in email_body:
-        if retry < _RETRIES:
-            time.sleep(_RETRY_DELAY)
-
-            return get_verification_code(response, username, retry + 1)
-        else:
-            raise TestFailError("No matching verification email could be validated after {} seconds.".format(_RETRIES * _RETRY_DELAY))
-
-    verification_code = email_body.split('verify?username={}&code='.format(username))[1].split('\n')[0].strip()
-
-    latest_key.delete()
-
-    response = {"email_verification_code": verification_code}
-    logger.info(response)
-
-    return response
+            raise TestFailError("Failed to retrieve verification email after {} seconds: {}".format(_RETRIES * _RETRY_DELAY, e))
